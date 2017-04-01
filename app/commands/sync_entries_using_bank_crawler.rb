799e7cfc351561a500b8c6b48b9a9aa1e6e916e7
Jaime Bunzli <jpbunzli@gmail.com>
2017-03-31 22:54:06 -0300

    feat(sync): sync bank crawler entries with local entries

---
+++ app/commands/sync_entries_using_bank_crawler.rb
@@ -0,0 +1,21 @@
+class SyncEntriesUsingBankCrawler < PowerTypes::Command.new(:product, :get_bank_crawler_command, :payload)
+  def perform
+    bank_entries = @get_bank_crawler_command.for(payload: @payload)
+    SignEntries.for(bank_entries: bank_entries)
+    create_new_entries(bank_entries)
+  end
+
+  private
+
+  def create_new_entries(bank_entries)
+    bank_entries.each do |bank_entry|
+      entry = Entry.find_or_create_by(product: @product, signature: bank_entry.signature)
+
+      entry.update_attributes({
+        description: bank_entry.description,
+        amount: bank_entry.amount,
+        date: bank_entry.date
+      })
+    end
+  end
+end
---
+++ spec/commands/sync_entries_using_bank_crawler_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+describe SyncEntriesUsingBankCrawler do
+  def perform(*_args)
+    described_class.for(*_args)
+  end
+
+  let(:payload) {
+    {
+      user_rut: "123",
+      company_rut: "1234",
+      password: "4321",
+      account_number: "1234"
+    }
+  }
+
+  let(:bank_entries) {
+    [
+      BankEntry.new(Date.new(2017, 3, 3), "pago en línea", 25000, :deposit),
+      BankEntry.new(Date.new(2017, 3, 4), "pago en línea 2", 25000, :deposit),
+      BankEntry.new(Date.new(2017, 3, 5), "pago en línea 3", 25000, :deposit)
+    ]
+  }
+
+  before do
+    create(:entry, product: "VISA CLP", signature: 12)
+    create(:entry, product: "VISA CLP", signature: 14)
+    create(:entry, product: "VISA USD", signature: 10)
+  end
+
+  before do
+    allow(bank_entries[0]).to receive(:signature).and_return(12)
+    allow(bank_entries[1]).to receive(:signature).and_return(10)
+    allow(bank_entries[2]).to receive(:signature).and_return(13)
+    allow(GetBancoDeChileCuentaCorrienteEntries).to receive(:for).and_return(bank_entries)
+  end
+
+  context "#perform" do
+    it "create only new entries" do
+      perform(product: "VISA CLP", get_bank_crawler_command: GetBancoDeChileCuentaCorrienteEntries, payload: payload)
+
+      expect(Entry.count).to eq(5)
+      expect(Entry.where(signature: 12).count).to eq(1)
+      expect(Entry.where(signature: 13).count).to eq(1)
+      expect(Entry.where(signature: 10).count).to eq(2)
+
+      entry = Entry.where(signature: 13).first
+      expect(entry.description).to eq(bank_entries[2].description)
+    end
+
+    it "create entries with correct data" do
+      perform(product: "VISA CLP", get_bank_crawler_command: GetBancoDeChileCuentaCorrienteEntries, payload: payload)
+
+      bank_entry = bank_entries[2]
+      entry = Entry.where(signature: 13).first
+
+      expect(entry.description).to eq(bank_entry.description)
+      expect(entry.amount).to eq(bank_entry.amount)
+      expect(entry.date).to eq(bank_entry.date)
+      expect(entry.product).to eq("VISA CLP")
+    end
+  end
+end
