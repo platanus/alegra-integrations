0d17a896107dbe60faf43c070f297f13aaa23bc3
Jaime Bünzli <jpbunzli@gmail.com>
2018-03-12 16:38:32 -0300

    feat(alegra): send buy type documents to alegra

--- app/commands/send_document_to_alegra.rb
+++ app/commands/send_document_to_alegra.rb
@@ -7,31 +7,36 @@ class SendDocumentToAlegra < PowerTypes::Command.new(:document)
   private
 
   def create_document
-    alegra_document = alegra_client.create_document(alegra_document_hash)
-
-    if alegra_document[:id]
-      @document.update_columns(alegra_id: alegra_document[:id], alegra_status: :synced)
+    if @document.document_type == :sale
+      alegra_document = alegra_client.create_document(alegra_document_hash)
     else
-      @document.update_columns(alegra_status: :error)
+      alegra_document = alegra_client.create_third_party_document(alegra_document_hash)
     end
 
+    update_document_alegra_id(alegra_document)
     alegra_document
   end
 
   def alegra_document_hash
-    {
+    hash = {
       id_client: @alegra_contact[:id].to_i,
       bill_number: @document.legal_id,
       bill_date: @document.date,
       bill_due_date: @document.date,
       price: @document.amount
     }
+
+    hash = hash.merge(category_id: 12) if @document.document_type == :buy
+
+    hash
   end
 
-  def date_formated(date)
-    month = date.month.to_s.length == 1 ? "0#{date.month}" : date.month
-    day = date.day.to_s.length == 1 ? "0#{date.day}" : date.day
-    "#{date.year}-#{month}-#{day}"
+  def update_document_alegra_id(alegra_document)
+    if alegra_document[:id]
+      @document.update_columns(alegra_id: alegra_document[:id], alegra_status: :synced)
+    else
+      @document.update_columns(alegra_status: :error)
+    end
   end
 
   def alegra_client
--- spec/commands/send_document_to_alegra_spec.rb
+++ spec/commands/send_document_to_alegra_spec.rb
@@ -5,6 +5,11 @@ describe SendDocumentToAlegra do
     described_class.for(*_args)
   end
 
+  before do
+    allow_any_instance_of(AlegraClient).to receive(:get_contact_by_rut).with(document.rut)
+                                                                           .and_return("id": 4)
+  end
+
   describe "#perform" do
     context "create sale document" do
       let(:document) do
@@ -22,9 +27,6 @@ describe SendDocumentToAlegra do
       end
 
       before do
-        allow_any_instance_of(AlegraClient).to receive(:get_contact_by_rut).with(document.rut)
-                                                                           .and_return("id": 4)
-
         allow_any_instance_of(AlegraClient).to receive(:create_document).with(document_hash)
                                                                         .and_return("id": 123)
 
@@ -33,5 +35,31 @@ describe SendDocumentToAlegra do
 
       it { expect(document.reload.alegra_id).to eq(123) }
     end
+  end
+
+  context "create buy document" do
+    let(:document) do
+      create(:document, :buy)
+    end
+
+    let(:document_hash) do
+      {
+        id_client: 4,
+        bill_number: document.legal_id,
+        bill_date: document.date,
+        bill_due_date: document.date,
+        price: document.amount,
+        category_id: 12
+      }
+    end
+
+    before do
+      allow_any_instance_of(AlegraClient).to receive(:create_third_party_document).with(document_hash)
+                                                                      .and_return("id": 123)
+
+      perform(document: document)
+    end
+
+    it { expect(document.reload.alegra_id).to eq(123) }
   end
 end
