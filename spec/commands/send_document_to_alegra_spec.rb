b0d113ed2abb95c07e9ee15e9f14e77214bcf4d2
Jaime Bünzli <jpbunzli@gmail.com>
2018-03-12 16:27:16 -0300

    chore(alegra): add tests to send buy documents

--- spec/commands/send_document_to_alegra_spec.rb
+++ spec/commands/send_document_to_alegra_spec.rb
@@ -5,5 +5,33 @@ describe SendDocumentToAlegra do
     described_class.for(*_args)
   end
 
-  pending "describe what perform does here"
+  describe "#perform" do
+    context "create sale document" do
+      let(:document) do
+        create(:document, :sale)
+      end
+
+      let(:document_hash) do
+        {
+          id_client: 4,
+          bill_number: document.legal_id,
+          bill_date: document.date,
+          bill_due_date: document.date,
+          price: document.amount
+        }
+      end
+
+      before do
+        allow_any_instance_of(AlegraClient).to receive(:get_contact_by_rut).with(document.rut)
+                                                                           .and_return("id": 4)
+
+        allow_any_instance_of(AlegraClient).to receive(:create_document).with(document_hash)
+                                                                        .and_return("id": 123)
+
+        perform(document: document)
+      end
+
+      it { expect(document.reload.alegra_id).to eq(123) }
+    end
+  end
 end
